11119d062f41be1a7b1c94ea11b097cd428674b8
Dima Gaponov <Gaponoffd@yandex.ru>
2018-08-20 21:48:47 +0300

    dz2

---
+++ dz2/index.php
@@ -0,0 +1,119 @@
+<?php
+///Задание 1
+function task1($paragraph1, $paragraph2 = false)
+{
+    if ($paragraph2) {
+        $var = "<p>".$paragraph1.$paragraph2."</p>";
+    } else {
+        $var = "<p>".$paragraph1."</p>";
+    }
+    return $var;
+};
+echo task1("Привет", " мир");
+echo task1("Привет");
+
+///Задание 2
+function task2($arg, $numbers)
+{
+    $data = null;
+    foreach ($numbers as $value) {
+        switch ($arg) {
+            case "+":
+                if (empty($data)) {
+                    $data = $value;
+                } else {
+                    $data += $value;
+                }
+                break;
+            case "-":
+                if (empty($data)) {
+                    $data = $value;
+                } else {
+                    $data -= $value;
+                }
+                break;
+            case "*":
+                if (empty($data)) {
+                    $data = $value;
+                } else {
+                    $data *= $value;
+                }
+                break;
+            case "/":
+                if (empty($data)) {
+                    $data = $value;
+                } else {
+                    $data /= $value;
+                }
+                break;
+        }
+    }
+    return $data;
+}
+$aaa = [10,5,2];
+echo task2("+", $aaa);
+echo "<br>";
+
+///Задание 3
+function task3($columns, $rows)
+{
+    if (!is_int($columns) || !is_int($rows)) {
+        echo "Задайте целое число";
+        return null;
+    }
+    if ($columns == 0 && $rows == 0) {
+        echo "Число не должно равняться нулю";
+        return null;
+    }
+    for ($i=1; $i<=$columns; $i++) {
+        echo'<tr>';
+        echo '<br>';
+        for ($j=1; $j<=$rows; $j++) {
+            $s=$j*$i;
+            echo'<td>'."($s)".'</td>';
+        }
+    }
+}
+task3(6, 2);
+echo "<br><br>";
+
+///Задание 4
+function task4()
+{
+    echo date("d/m/Y");
+}
+task4();
+echo "<br><br>";
+function task5($date)
+{
+    echo strtotime($date);
+}
+task5("24.02.2016 00:00:00");
+echo "<br><br>";
+
+///Задание 5
+function task6()
+{
+    $string = "Карл у Клары украл Кораллы";
+    echo str_replace("К", "", $string);
+}
+task6();
+echo "<br><br>";
+
+function task7()
+{
+    $str = "Две бутылки лимонада";
+    echo str_replace("Две", "Три", $str);
+}
+task7();
+echo "<br><br>";
+
+///Задание 6
+function task8($name)
+{
+    $string = "Hello again!";
+    $test = fopen($name, "w+");
+    fwrite($test, $string);
+    echo file_get_contents($name);
+}
+task8("test.txt");
