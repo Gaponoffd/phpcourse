c801b0b881ace521711a61c5a1710ce0dbc8cd11
Dima Gaponov <Gaponoffd@yandex.ru>
2018-08-28 22:22:40 +0300

    ВП1 contin

--- burgers/order.php
+++ burgers/order.php
@@ -5,20 +5,46 @@ if (mysqli_connect_errno()) {
     exit();
 }
 
-$email = $_REQUEST["email"];
-$name = $_REQUEST["name"];
-$phone = $_REQUEST["phone"];
-$street = $_REQUEST["street"];
-$home = $_REQUEST["home"];
-$housing = $_REQUEST["housing"];
-$apartment = $_REQUEST["apartment"];
-$floor = $_REQUEST["floor"];
-$comment = $_REQUEST["comment"];
+//$email = $_REQUEST["email"];
+//$name = $_REQUEST["name"];
+//$phone = $_REQUEST["phone"];
+//$street = $_REQUEST["street"];
+//$home = $_REQUEST["home"];
+//$housing = $_REQUEST["housing"];
+//$apartment = $_REQUEST["apartment"];
+//$floor = $_REQUEST["floor"];
+//$comment = $_REQUEST["comment"];
+$email = 'уцуцa';
+$name = 'дима';
+$phone = '8888';
 
-$sql = "INSERT INTO users (email, name, phone) VALUES ('$email', '$name','$phone')";
-$result = $mysqli->query($sql);
 
-$sql2 = "INSERT INTO order (street, home, housing, apartment, floor, comment) 
-VALUES ('$street', '$home', '$housing', '$apartment', '$floor', '$comment')";
-$result2 = $mysqli->query($sql2);
-echo "Привет";
+echo "<pre>";
+
+$been = "SELECT email FROM `users`";
+$been = "SELECT email FROM `users`";
+$beenSqli = $mysqli->query($been);
+$beenFetch = $beenSqli -> fetch_all();
+
+//$numberOrders=1;
+if($beenFetch==array()) {
+    $sql = "INSERT INTO `users` (email, name, phone, number_orders) VALUES ('$email', '$name','$phone', '1')";
+    $result = $mysqli->query($sql);
+//    $numberOrders = 1;
+} else {
+    $numberOrders += $numberOrders;
+    foreach ($beenFetch as $item) {
+        if ($item[0] == $email) {
+            $sqlNub = "UPDATE users SET number_orders = '$item[4]+1'";
+            $result = $mysqli->query($sqlNub);
+        }
+        print_r($beenFetch);
+    }
+}
+//echo $numberOrders;
+
+//$sql2 = "INSERT INTO `order` (street, home, housing, apartment, floor, comment)
+//VALUES ('$street', '$home', '$housing', '$apartment', '$floor', '$comment')";
+//$result2 = $mysqli->query($sql2);
+
+//print_r($been3);
