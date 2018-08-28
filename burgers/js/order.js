58ba0082d0e959ceb9f2fe8b547c0572ee0c3b4b
Dima Gaponov <Gaponoffd@yandex.ru>
2018-08-28 11:19:26 +0300

    ВП1 contin

--- burgers/js/order.js
+++ burgers/js/order.js
@@ -3,12 +3,25 @@ $('.order__form-button').on('click', function (e) {
     var name = $('#name').val();
     var phone = $('#phone').val();
     var email = $('#email').val();
+    var street = $('#street').val();
+    var home = $('#home').val();
+    var housing = $('#housing').val();
+    var apartment = $('#apartment').val();
+    var floor = $('#floor').val();
+    var comment = $('#comment').val();
+
     $.ajax({
         url: './order.php',
         data: {
             name: name,
             phone: phone,
-            email: email
+            email: email,
+            street: street,
+            home: home,
+            housing: housing,
+            apartment: apartment,
+            floor: floor,
+            comment: comment
         },
         method: 'POST'
     }).done(function (data) {
--- burgers/order.php
+++ burgers/order.php
@@ -4,10 +4,21 @@ if (mysqli_connect_errno()) {
     printf("Ошибка соединения:%s\n", mysqli_connect_error());
     exit();
 }
+
 $email = $_REQUEST["email"];
 $name = $_REQUEST["name"];
 $phone = $_REQUEST["phone"];
+$street = $_REQUEST["street"];
+$home = $_REQUEST["home"];
+$housing = $_REQUEST["housing"];
+$apartment = $_REQUEST["apartment"];
+$floor = $_REQUEST["floor"];
+$comment = $_REQUEST["comment"];
 
-$sql = "INSERT INTO users (email, name, phone) VALUES ('$email', '$name', '$phone')";
+$sql = "INSERT INTO users (email, name, phone) VALUES ('$email', '$name','$phone')";
 $result = $mysqli->query($sql);
+
+$sql2 = "INSERT INTO order (street, home, housing, apartment, floor, comment) 
+VALUES ('$street', '$home', '$housing', '$apartment', '$floor', '$comment')";
+$result2 = $mysqli->query($sql2);
 echo "Привет";
